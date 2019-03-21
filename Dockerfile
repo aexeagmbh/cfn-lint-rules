82635f76b317618115f06ed26579969ebbb58ee2
ttb <git@hackeroase.de>
2019-03-21 15:55:48 +0100

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.7-stretch
+
+COPY requirements.txt /tmp
+RUN pip install --no-cache-dir -r /tmp/requirements.txt
+RUN rm /tmp/requirements.txt
+
+RUN useradd uid1000 --home-dir /home/uid1000 --uid 1000
+RUN mkdir -p /home/uid1000 && chown uid1000: /home/uid1000
+USER uid1000
+
+WORKDIR /usr/src/app
+COPY ./ .
---
+++ rules/s3_block_public_access.py
@@ -0,0 +1,50 @@
+from cfnlint import CloudFormationLintRule, RuleMatch
+
+
+class S3BucketPublicAccess(CloudFormationLintRule):
+    """Rule description """
+
+    id = "W9301"
+    shortdesc = "Enable S3 Block Public Access"
+    description = "Enable S3 Block Public Access"
+    source_url = "https://aws.amazon.com/about-aws/whats-new/2018/11/introducing-amazon-s3-block-public-access/"
+    tags = ["s3", "security"]
+
+    def match(self, cfn):
+        """Basic Rule Matching"""
+
+        matches = []
+
+        resources = cfn.get_resources(["AWS::S3::Bucket"])
+        for resource_name, resource in resources.items():
+            properties = resource.get("Properties", {})
+            public_access_block_configuration = properties.get(
+                "PublicAccessBlockConfiguration"
+            )
+            path = [
+                "Resources",
+                resource_name,
+                "Properties",
+                "PublicAccessBlockConfiguration",
+            ]
+            if not public_access_block_configuration:
+                message = f"Property {'/'.join(path)} is missing"
+                matches.append(RuleMatch(path, message))
+                continue
+            for key in [
+                "BlockPublicAcls",
+                "BlockPublicPolicy",
+                "IgnorePublicAcls",
+                "RestrictPublicBuckets",
+            ]:
+                value = public_access_block_configuration.get(key)
+                if value is None:
+                    detail_path = path + [key]
+                    message = f"Property {'/'.join(detail_path)} is missing and should be true"
+                    matches.append(RuleMatch(detail_path, message))
+                elif value is not True:
+                    detail_path = path + [key]
+                    message = f"Property {'/'.join(detail_path)} should be true"
+                    matches.append(RuleMatch(detail_path, message))
+
+        return matches
