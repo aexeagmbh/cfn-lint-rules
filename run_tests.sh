9355d02bbb8de03177582e7fbaa3e0eb95aa3f0c
ttb <git@hackeroase.de>
2021-11-17 15:44:30 +0100

    [run_tests] better test order

--- run_tests.sh
+++ run_tests.sh
@@ -3,11 +3,11 @@
 set -ex
 
 
-pytest
 black .
 isort .
 pylama
 mypy .
-yamllint tests/good/*.yaml tests/bad/*.yaml docker-compose.yml .yamllint .cfnlintrc .github/workflows/*.yml
+yamllint --strict tests/good/*.yaml tests/bad/*.yaml docker-compose.yml .yamllint .cfnlintrc .github/workflows/*.yml
+pytest
 python3 -m build --sdist --wheel .
 python3 -m twine check dist/*
