d4f3fb783cf5eee418731681750faa26af7efd0a
ttb <git@hackeroase.de>
2022-09-21 22:21:52 +0200

    Release 0.0.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="cfn_lint_ax",
-    version="0.0.8",
+    version="0.0.9",
     author="AX Semantics",
     author_email="infrastructure@ax-semantics.com",
     description="Rules for cfn lint.",
