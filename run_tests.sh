5798397d2bda2697b925a3252210138270f80e43
ttb <git@hackeroase.de>
2022-09-21 21:44:06 +0200

    [pylint] disable duplicate-code checks

--- run_tests.sh
+++ run_tests.sh
@@ -6,6 +6,7 @@ set -ex
 black .
 isort .
 python -m flake8 .
+python -m pytest --pylint -m pylint cfn_lint_ax --pylint-rcfile=$(pwd)/pyproject.toml --pylint-jobs=0
 mypy .
 yamllint --strict tests/good/*.yaml tests/bad/*.yaml docker-compose.yml .yamllint .cfnlintrc .github/workflows/*.yml
 pytest
