24660238a872f6c79714c6b21a1fe6149bdc1d18
ttb <git@hackeroase.de>
2024-07-01 17:26:32 +0200

    v0.0.20

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="cfn_lint_ax",
-    version="0.0.19",
+    version="0.0.20",
     author="AX Semantics",
     author_email="infrastructure@ax-semantics.com",
     description="Rules for cfn lint.",
